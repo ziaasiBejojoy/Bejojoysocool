e2cc889112f9c04a0ade3f7f896fb584eebd63ad
unknown <ziziinginsukses@gmail.com>
2026-03-25 17:38:52 +0700

    update1

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -52,7 +52,7 @@ export default function HeroSection() {
               Welcome to my personal portfolio
             </motion.span>
 
-            <motion.h1
+            <motion.h2
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.3 }}
@@ -61,9 +61,9 @@ export default function HeroSection() {
               Zia Syarafana Akbar
               <br />
               <span className="text-gradient">
-                | Student, Artist & Social Science Enthusiast
+              Student, Artist & Social Science Enthusiast
               </span>
-            </motion.h1>
+            </motion.h2>
 
             <motion.p
               initial={{ opacity: 0, y: 30 }}
